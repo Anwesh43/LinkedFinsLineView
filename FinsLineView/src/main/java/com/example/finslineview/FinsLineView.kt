28eaf6df84abd901ff57ea536d0208939b905f10
Anwesh43 <anweshthecool0@gmail.com>
2021-04-30 04:33:10 +0530

    adding renderer in view

--- FinsLineView/src/main/java/com/example/finslineview/FinsLineView.kt
+++ FinsLineView/src/main/java/com/example/finslineview/FinsLineView.kt
@@ -59,14 +59,16 @@ fun Canvas.drawFLNode(i : Int, scale : Float, paint : Paint) {
 
 class FinsLineView(ctx : Context) : View(ctx) {
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer : Renderer = Renderer(this)
 
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
