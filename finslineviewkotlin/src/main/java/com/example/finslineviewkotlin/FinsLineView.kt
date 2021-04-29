b46855b0e68e657e790c0c9adfc6657bcc01f9f2
Anwesh43 <anweshthecool0@gmail.com>
2021-04-30 04:50:01 +0530

    adding new module for view

--- finslineviewkotlin/src/main/java/com/example/finslineviewkotlin/FinsLineView.kt
+++ finslineviewkotlin/src/main/java/com/example/finslineviewkotlin/FinsLineView.kt
@@ -1,2 +1,219 @@
 package com.example.finslineviewkotlin
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.Canvas
+import android.content.Context
+import android.app.Activity
+
+val colors : Array<String> = arrayOf(
+    "#f44336",
+    "#9C27B0",
+    "#BF360C",
+    "#00C853",
+    "#1A237E"
+)
+val parts : Int = 4
+val strokeFactor : Float = 90f
+val scGap : Float = 0.02f / parts
+val sizeFactor : Float = 2.9f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val rot : Float = 45f
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawFinsLine(scale : Float, w : Float, h : Float, paint : Paint) {
+    val sf : Float = scale.sinify()
+    val gap : Float = w / parts
+    save()
+    translate(0f, h / 2)
+    for (j in 0..(parts - 1)) {
+        save()
+        translate((j + 1) * gap, 0f)
+        for (k in 0..2) {
+            save()
+            rotate(rot * (k - 1) * sf.divideScale(parts + j, parts * 2))
+            drawLine(-gap , 0f, -gap + gap * sf.divideScale(j, 2 * parts), 0f, paint)
+            restore()
+        }
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawFLNode(i : Int, scale : Float, paint : Paint) {
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawFinsLine(scale, w, h, paint)
+}
+
+class FinsLineView(ctx : Context) : View(ctx) {
+
+    private val renderer : Renderer = Renderer(this)
+
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas)
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                renderer.handleTap()
+            }
+        }
+        return true
+    }
+
+    data class State(var scale : Float = 0f, var dir : Float = 0f, var prevScale : Float = 0f) {
+
+        fun update(cb : (Float) -> Unit) {
+            scale += dir * scGap
+            if (Math.abs(scale - prevScale) > 1) {
+                scale = prevScale + dir
+                dir = 0f
+                prevScale = scale
+                cb(prevScale)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            if (dir == 0f) {
+                dir = 1f - 2 * prevScale
+                cb()
+            }
+        }
+    }
+
+    data class Animator(var view : View, var animated : Boolean = false) {
+
+        fun animate(cb : () -> Unit) {
+            if (animated) {
+                cb()
+                try {
+                    Thread.sleep(delay)
+                    view.invalidate()
+                } catch(ex : Exception) {
+
+                }
+            }
+        }
+
+        fun start() {
+            if (!animated) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
+
+        fun stop() {
+            if (animated) {
+                animated = false
+            }
+        }
+    }
+
+    data class FLNode(var i : Int, val state : State = State()) {
+
+        private var next : FLNode? = null
+        private var prev : FLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = FLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawFLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : FLNode {
+            var curr : FLNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
+
+    data class FinsLine(var i : Int) {
+
+        private var curr : FLNode = FLNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
+
+    data class Renderer(var view : FinsLineView) {
+
+        private val animator : Animator = Animator(view)
+        private val finsLine : FinsLine = FinsLine(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            finsLine.draw(canvas, paint)
+            animator.animate {
+                finsLine.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            finsLine.startUpdating {
+                animator.start()
+            }
+        }
+    }
+
+    companion object {
+
+        fun create(activity : Activity) : FinsLineView {
+            val view : FinsLineView = FinsLineView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
+}
